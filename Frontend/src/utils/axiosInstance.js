a2f599f0d903833ecb4179fe650c5eb0b878a535
Gattupalli Hema Venkata Naga Mani Deep <ghvnmanideep@gmail.com>
2025-09-24 18:28:17 +0530

    Update axiosInstance.js

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://let-sblog.onrender.com',
+  baseURL: import.meta.env.VITE_API_URL || 'https://let-sblog.onrender.com/',
 });
 
 instance.interceptors.request.use(config => {
